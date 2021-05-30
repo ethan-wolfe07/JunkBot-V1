7fc2d18069342b70ccce620ebe37b9fad2ef92da
JengaDevEzE <75378662+JengaDevEzE@users.noreply.github.com>
2021-05-29 20:26:57 -0400

    Updating Spacing

--- objects/Fun/say.js
+++ objects/Fun/say.js
@@ -9,13 +9,11 @@ module.exports = {
     expectedArgs: '<message>',
     callback: (client, message, arguments, emb) => {
 
-
         for(const c in blacklistedWords) {
             if(arguments.includes(blacklistedWords[c])) {
                 return message.channel.send('I cannot send your message as it contains words that are blacklisted!')
             }
         }
-
 
         let msg = arguments.slice(1).join(" ") // Putting it into one variable since they will always have at least one word
         message.channel.send(msg) // Sending it to the same channel they are in. Change the way it works if you want.
